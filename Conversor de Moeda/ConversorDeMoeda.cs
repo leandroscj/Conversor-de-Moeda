62bfd46df927a08cccdb476d95ba7269bee6d8f5
Leandro Sousa <leandroscjTI@gmail.com>
2020-07-08 15:28:44 -0300

    Add project files.

---
+++ Conversor de Moeda/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿namespace Conversor_de_Moeda
+{
+    class ConversorDeMoeda
+    {
+        static public double Iof = 1.1;
+
+
+        static public double Total(double cotacao, double quantidade)
+        {
+            double total =  cotacao * quantidade;
+            return total + total * Iof / 100;
+        }
+    }
+    
+}
+    
---
+++ Conversor de Moeda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Conversor_de_Moeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares vai comprar?");
+            double quantidade = double.Parse(Console.ReadLine());
+
+            double result = ConversorDeMoeda.Total(quantidade, cotacao);
+            Console.Write(result.ToString("f2"));
+
+        }
+
+    }
+}
